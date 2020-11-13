09d9f2f857fd5f8a4dd795771c0f0e46fc37c531
Jhon Freddy Puentes <=>
2020-11-12 20:42:14 -0500

    ADD: Agrego TODO, nuevos parametros e iniciacion de metricas de distancia

--- spanlp/palabrota.py
+++ spanlp/palabrota.py
@@ -17,17 +17,27 @@ _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 class Palabrota():
+    # TODO documentar
     _words = None
     _censor_chars = '@#$%!'
     _censor_pool = []
     _countries = ["COL", "VEN"]
+    _exclude = []
+    _include = []
+    _distance_metric = None
 
-    def __init__(self, censor_char=None, countries=None) -> None:
+    def __init__(self, censor_char=None, countries=None, exclude=None, include=None, distance_metric=None) -> None:
         super().__init__()
         if censor_char:
             self._censor_chars = censor_char
         if countries:
             self._countries = countries
+        if exclude:
+            self._exclude = exclude
+        if include:
+            self._include = include
+        if distance_metric:
+            self._distance_metric = distance_metric
 
     def __str__(self) -> str:
         return """\
@@ -39,12 +49,14 @@ class Palabrota():
         return os.path.join(_ROOT, 'dataset', path)
 
     def set_censor_characters(self, new_censor_chars) -> None:
+        # TODO documentar
         global _censor_chars
         if len(new_censor_chars) == 0:
             new_censor_chars = " "
         self._censor_chars = new_censor_chars
 
     def __load_words(self, country=None) -> None:
+        # TODO documentar
         # TODO mejorar forma de almacenar los datos con que se trabajara. Pasarlo a memoria.
         global _words
         word_list = []
@@ -67,6 +79,7 @@ class Palabrota():
         except Exception as e:
             print(e)
             word_list = []
+        # TODO validar/usar exclude, include
         _words = word_list
 
     def __get_words(self, country=None) -> list:
@@ -79,6 +92,8 @@ class Palabrota():
         return self._censor_pool.pop(random.randrange(len(self._censor_pool)))
 
     def censor(self, input_text, country=None) -> str:
+        # TODO documentar
+        # TODO validar/usar exclude, include
         ret = input_text
         words = self.__get_words(country=country)
         for word in words:
@@ -88,4 +103,13 @@ class Palabrota():
         return ret
 
     def contains_palabrota(self, text) -> bool:
+        # TODO documentar
+        # TODO validar/usar exclude, include
         return text != self.censor(text)
+
+    # Metricas de distancias:
+    # Ver: https://www.kdnuggets.com/2019/01/comparison-text-distance-metrics.html
+    def levenshtein(self):
+        # TODO documentar
+        # TODO convertir en un modulo aparte.
+        pass
