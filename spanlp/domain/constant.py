9b7c7502c82ba6f38656d57cf35ee2331fa6479b
Jhon Freddy Puentes <=>
2020-12-04 21:48:39 -0500

    ADD: Implementacion de 28 estrategias de pre-procesamiento y limpieza de datos. Correccion de BUG #1 (Soportar palabras con numeros)

---
+++ spanlp/domain/constant.py
@@ -0,0 +1,109 @@
+SPANISH_STOP_WORDS = ["hola", "chao", "a", "al", "algo", "algunas", "algunos", "ante", "antes", "como", "con", "contra", "cual", "cuando", "de"
+, "del", "desde", "donde", "durante", "e", "ella", "ellas", "ellos", "en", "entre", "era", "erais", "eran", "eras", "eres",
+"es", "esa", "esas", "ese", "eso", "esos", "esta", "estaba", "estabais", "estaban", "estabas", "estad", "estada", "estadas",
+"estado", "estados", "estamos", "estando", "estar", "estaremos", "estara", "estaran", "estaras", "estare", "estareis", "estaria",
+"estariais", "estariamos", "estarian", "estarias", "estas", "este", "estemos", "esto", "estos", "estoy", "estuve", "estuviera",
+ "estuvierais", "estuvieran", "estuvieras", "estuvieron", "estuviese", "estuvieseis", "estuviesen", "estuvieses", "estuvimos", "estuviste",
+"estuvisteis", "estuvieramos", "estuviesemos", "estuvo", "esta", "estabamos", "estais", "estan", "estas", "este", "esteis", "esten",
+"estes", "fue", "fuera", "fuerais", "fueran", "fueras", "fueron", "fuese", "fueseis", "fuesen", "fueses", "fui", "fuimos", "fuiste", "fuisteis",
+"fueramos", "fuesemos", "ha", "habida", "habidas", "habido", "habidos", "habiendo", "habremos", "habra", "habran", "habras", "habre", "habreis",
+"habria", "habriais", "habriamos", "habrian", "habrias", "habeis", "habia", "habiais", "habiamos", "habian", "habias", "han", "has", "hasta", "hay",
+"haya", "hayamos", "hayan", "hayas", "hayais", "he", "hemos", "hube", "hubiera", "hubierais", "hubieran", "hubieras", "hubieron", "hubiese", "hubieseis",
+"hubiesen", "hubieses", "hubimos", "hubiste", "hubisteis", "hubieramos", "hubiesemos", "hubo", "la", "las", "le", "les", "lo", "los", "me", "mi", "mis",
+"mucho", "muchos", "muy", "mas", "mi", "mia", "mias", "mio", "mios", "nada", "ni", "no", "nos", "nosotras", "nosotros", "nuestra", "nuestras", "nuestro",
+"nuestros", "o", "os", "otra", "otras", "otro", "otros", "para", "pero", "poco", "por", "porque", "que", "quien", "quienes", "se", "sea", "seamos", "sean",
+"seas", "seremos", "sera", "seran", "seras", "sere", "sereis", "seria", "seriais", "seriamos", "serian", "serias", "seais", "sido", "siendo", "sin", "sobre",
+"sois", "somos", "son", "soy", "su", "sus", "suya", "suyas", "suyo", "suyos", "si", "tambien", "tanto", "te", "tendremos", "tendra", "tendran", "tendras", "tendre",
+"tendreis", "tendria", "tendriais", "tendriamos", "tendrian", "tendrias", "tened", "tenemos", "tenga", "tengamos", "tengan", "tengas", "tengo", "tengais",
+"tenida", "tenidas", "tenido", "tenidos", "teniendo", "teneis", "tenia", "teniais", "teniamos", "tenian", "tenias", "ti", "tiene", "tienen", "tienes", "todo",
+"todos", "tu", "tus", "tuve", "tuviera", "tuvierais", "tuvieran", "tuvieras", "tuvieron", "tuviese", "tuvieseis", "tuviesen", "tuvieses", "tuvimos", "tuviste",
+"tuvisteis", "tuvieramos", "tuviesemos", "tuvo", "tuya", "tuyas", "tuyo", "tuyos", "tu", "un", "una", "uno", "unos", "vosotras", "vosotros", "vuestra", "vuestras",
+"vuestro", "vuestros", "y", "ya", "yo", "el", "eramos"]
+
+
+ARTICLES = ["el", "la", "los", "las", "un", "una", "unos", "unas"]
+
+
+PRONOUNS = ["yo", "tu", "vos", "el", "ella", "ello", "usted", "nosotros", "nosotras"]
+
+
+ADVERBS = ["ahora", "antes", "despues", "tarde", "luego", "ayer", "hoy", "manana", "todavia", "aun", "nunca", "enseguida", "mientras", "despues", "temprano",
+"aqui", "lejos", "cerca", "fuera", "detras", "encima", "ahi", "arriba", "bien", "mal", "lentamente", "segun", "mejor", "peor",
+"tanto", "mucho", "demasiado", "mas", "poco", "menos", "nada", "todo", "no", "tampoco", "nada", "nunca", "jamas", "si", "tambien", "asimismo", "bueno",
+"seguro", "naturalmente", "verdaderamente"]
+
+CONJUNCTIONS = ["y", "e", "ni", "que", "pero", "mas", "aunque", "sino", "siquiera", "o", "u", "ora", "sea", "bien", "ya", "cerca", "lejos", "este", "aquel"]
+
+
+PREPROSITIONS = ["a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "durante", "en", "entre", "hacia", "hasta", "mediante", "para", "por", "segun",
+ "sin", "so", "sobre", "tras", "versus", "via"]
+
+
+EMOTICONS = [".|.", ":‑)", ":-]", ":)", ":]", ":-3", ":3", ":->", ":>", "8-)", "8)", ":-}", ":}", ":o)", ":c)", ":^)", "=]", "=)",
+ ":‑D", ":D", "8‑D", "8D", "x‑D", "xD", "X‑D", "XD", "=D", "=3", "B^D", ":-))", ":‑(", ":(", ":‑c", ":c", ":‑<", ":<", ":‑[", ":[",
+":-||", ">:[", ":{", ":@", ":(", ";(", ":'‑(", ":'(", ":'‑)", ":')", "D‑':", "D:<", "D:", "D8", "D;", "D=", "DX", ":‑O",
+":O", ":o", ":‑o", ":-0", "8‑0", ">:O", ":-*", ":*", ":×", ";‑)", ":×", ";‑)", "*-)", "*)", ";)", "*-)", ";‑]", ";]", ";^)", ":‑,",
+ ";D", ":‑P", "X‑P", ":P", "XP", "<3", "><>", "<*)))‑{",
+ "x‑p", "xp", ":‑p", ":p", ":‑Þ", ":Þ", ":‑þ", ":þ", ":‑b", ":b", "d:", "=p", ">:P", ":‑/", ":/", ":‑.", ">:\\", ">:/", ":\\",
+ "=/", "=\\", ":L", "=L", ":S", ":‑|", ":|", ":$", "://)", "://3", ":‑X", ":X",
+  ":‑#", ":#", ":‑&", ":&", "O:‑)", "O:)", "0:‑3", "0:3", "0:‑)", "0:)", "0;^)", ">:‑)", ">:)", "}:‑)", "}:)", "3:‑)", "3:)",
+  ">;)", ">:3", ";3", "|;‑)", "|‑O", ":‑J", "#‑)", "%‑)", "%)", ":‑###..", ":###..", "<:‑|", "',:-|", "',:-l", ":E",
+   "@};-", "@}->--", "@}‑;‑'‑‑‑", "@>‑‑>‑‑", "5:‑)", "~:‑\\", "*<|:‑)", "~(_8^(I)", "=:o]", "7:^]", ",:‑)", "</3", "<\\3",
+   "><(((*>", "\o/", "*\\0/*", "//0‑0\\\\", "v.v", "O_O", "o‑o", "O_o", "o_O", "o_o", "O-O", ">.<", "^5", "o/\o", ">_>^ ^<_<", ":-)"]
+
+
+ADJECTIVES = ["simpatico", "lento", "rigidos","simple", "tenebrosa", "habil","limpio", "fuerte", "especial","impulsivo",
+ "intrepida", "amable","ansioso", "redondo", "moderno","ruidoso", "pequeña", "delgada","irresponsable", "grande", "maduro"
+ ,"enfermo", "grandes", "curioso" ,"nueva", "cordial", "alto","sutil", "pequeño", "monotonos","amarillento", "estudioso", "modesto"
+ ,"entrometida", "vulgar", "feliz","feo", "antiguos", "simples","verde", "extraordinario", "imperfecto","inteligentes", "generoso",
+ "maravilloso","fria", "ardiente", "distante","sensibles", "apasionado", "mediano","negro", "nuevo", "ansiada","colorido", "odioso",
+ "cuidadoso","blanca", "ambicioso",  "extrovertida", "infantil", "rojo", "oscura",
+ "obsecuente","arrugado", "bellos", "viejo","sencillos", "agradable", "gordo","dura", "desordenado", "horrible","prolijo", "pacifico", "rosado","refinado", "acida", "duro","celoso", "alargado", "angelical","estresante", "contento", "sucio","valiente", "enfermiza", "lindo","transparente", "flaco", "fragil"
+ ,"gris", "incomoda", "diferente","triste", "corto", "fugaz","crocante", "engreidas", "ancha","debil", "libre", "timida","caliente",
+ "agotador", "complicados","calido", "desconfiado", "quebrad","bonito", "realista", "suave","complejos", "chiquito", "azul","violeta", "azulado", "cansado","marron", "peludo", "cuadrado","apatico", "luminoso", "coherente","helado", "amarillo", "malo","interesante", "brillante", "aburrido","directo", "cultos", "atento",
+ "alegres", "cauto", "afectuoso","insensible", "inmensas", "malcriado","grueso", "agil", "dulce","burlon", "ordenado", "celeste",
+ "comodo", "abstracto", "considerado","rapido", "sobrio", "negra","cruel", "largo", "dificil","ancho", "amargado", "perfecto"]
+
+
+ABBREVIATIONS = {
+ "acm": "haceme",
+ "alv": "a la verga",
+ "asc": "al salir de clase",
+ "bn": "bien",
+ "xo": "pero",
+ "xa": "para",
+ "xq": "por que",
+ "pq": "porque",
+ "sbs": "sabes",
+ "b7s": "besitos",
+ "gsp": "gente superpendeja",
+ "esk": "es que",
+ "fvr": "favor",
+ "k": "que",
+ "q": "que",
+ "1o": "primero",
+ "1r": "primer",
+ "d": "de",
+ "d nd":"de nada",
+ "d+": "demás",
+ "dnd": "donde",
+ "nls": "no lo se",
+ "kriño": "cariño",
+ "kyat": "callate",
+ "msj": "mensaje",
+ "mñn": "mañana",
+ "es3": "estres",
+ "tb": "tambien",
+ "tp": "tampoco",
+ "tqm": "te quiero mucho",
+ "to2": "todos",
+ "tvl": "te veo luego",
+ "wno": "bueno",
+ "weno": "bueno",
+ "wapo": "guapo",
+ "xas": "muchas",
+ "chas": "muchas",
+ "xfa": "por favor",
+ "porfa": "por favor",
+ "salu2": "saludos"
+}
--- spanlp/domain/strategies.py
+++ spanlp/domain/strategies.py
@@ -1,21 +1,42 @@
 # -*- coding: utf-8 -*-
 import enum
+from .constant import SPANISH_STOP_WORDS, ARTICLES, EMOTICONS, PRONOUNS, \
+    ADVERBS, CONJUNCTIONS, PREPROSITIONS, ADJECTIVES, ABBREVIATIONS
+from abc import abstractmethod
+import re
+import string
 
 
-class Strategy(enum.Enum):
+class DistanceMetricStrategy(enum.Enum):
     JaccardIndex = 'JaccardIndex'
     CosineSimilarity = 'CosineSimilarity'
+
+
+class CleanDataStrategy:
+
+    @classmethod
+    def remove_dynamic_tokens(cls, data: str, words: [str] = None):
+        if data:
+            result = ' '.join([word for word in data.split(' ') if word not in words])
+            return result
+        return ""
+
+    @abstractmethod
+    def clean(self, data: str):
+        pass
 
 
 class TextDistanceStrategy(object):
     name = ''
     threshold = 0.0
     normalize_text = False
+    clean_strategies: [CleanDataStrategy] = None
 
-    def __init__(self, name, threshold, normalize=False) -> None:
+    def __init__(self, name, threshold, normalize=False, clean_strategies: [CleanDataStrategy] = None) -> None:
         self.name = name
         self.threshold = threshold
         self.normalize_text = normalize
+        self.clean_strategies = clean_strategies
 
     def calculate(self, word1: str, word2: str) -> float:
         pass
@@ -31,14 +52,14 @@ class TextDistanceStrategy(object):
 
         return cw, sw, lw
 
-    @classmethod
-    def normalize(cls, text: str):
+    def normalize(self, text: str):
         if len(text.strip()) == 0:
             return ""
-        text = text.lower()
-        text = text.replace("  ", " ").replace("   ", " ")
-        text = text.replace("á", "a").replace("é", "e").replace("í", "i")\
-            .replace("ó", "o").replace("ú", "u").replace("ñ", "n")
+        if self.normalize_text and self.clean_strategies and len(self.clean_strategies) > 0:
+            for clean_strategy in self.clean_strategies:
+                text = clean_strategy.clean(text)
+        if self.normalize_text and (self.clean_strategies is None or len(self.clean_strategies) == 0):
+            raise Exception("Si indica normalize=True debe enviar al menos una estrategia de limpieza de datos.")
         return text
 
     def __str__(self):
@@ -48,8 +69,9 @@ class TextDistanceStrategy(object):
 class JaccardIndex(TextDistanceStrategy):
     n_gram = 2
 
-    def __init__(self, threshold=0.8, normalize=False, n_gram=2) -> None:
-        super().__init__(Strategy.JaccardIndex.name, threshold, normalize=normalize)
+    def __init__(self, threshold=0.8, n_gram=2, normalize=False, clean_strategies: [CleanDataStrategy] = None) -> None:
+        super().__init__(name=DistanceMetricStrategy.JaccardIndex.name, threshold=threshold, normalize=normalize,
+                         clean_strategies=clean_strategies)
         self.n_gram = n_gram
 
     def calculate(self, word1: str, word2: str) -> float:
@@ -94,8 +116,9 @@ class JaccardIndex(TextDistanceStrategy):
 
 class CosineSimilarity(TextDistanceStrategy):
 
-    def __init__(self, threshold=0.9, normalize=False) -> None:
-        super().__init__(Strategy.CosineSimilarity.name, threshold, normalize)
+    def __init__(self, threshold=0.9, normalize=False, clean_strategies: [CleanDataStrategy] = None) -> None:
+        super().__init__(name=DistanceMetricStrategy.CosineSimilarity.name, threshold=threshold, normalize=normalize,
+                         clean_strategies=clean_strategies)
 
     def calculate(self, word1: str, word2: str) -> float:
         if self.normalize_text:
@@ -110,3 +133,234 @@ class CosineSimilarity(TextDistanceStrategy):
     def cosine_distance(cls, v1, v2):
         common = v1[1].intersection(v2[1])
         return sum(v1[0][ch] * v2[0][ch] for ch in common) / v1[2] / v2[2]
+
+
+class Preprocessing:
+
+    def __init__(self, data: str = None, clean_strategies: [CleanDataStrategy] = None) -> None:
+        self._text = data
+        self._clean_strategies = clean_strategies
+
+    def clean(self, data: str = None, clean_strategies: [CleanDataStrategy] = None):
+        if data is not None:
+            self._text = data
+
+        if clean_strategies is not None:
+            self._clean_strategies = clean_strategies
+
+        if self._text is None or len(self._text.strip()) == 0:
+            return ""
+
+        if self._clean_strategies and len(self._clean_strategies) > 0:
+            for clean_strategy in self._clean_strategies:
+                self._text = clean_strategy.clean(self._text)
+            return self._text
+        else:
+            raise Exception("Debe enviar al menos una estrategia de pre-procesamiento o limpieza de datos.")
+
+
+class TextToLower(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.lower()
+        return ""
+
+
+class TextToUpper(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.upper()
+        return ""
+
+
+class RemoveUnicodeCharacters(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.encode('ascii', 'ignore').decode()
+        return ""
+
+
+class NumbersToVowelsInLowerCase(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("4", "a").replace("3", "e").replace("1", "i").replace("0", "o")
+        return ""
+
+
+class NumbersToVowelsInUpperCase(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("4", "A").replace("3", "E").replace("1", "I").replace("0", "O")
+        return ""
+
+
+class NumbersToConsonantsInLowerCase(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("9", "g").replace("7", "t").replace("5", "s").replace("2", "z")\
+                .replace("1", "l").replace("6", "g")
+        return ""
+
+
+class NumbersToConsonantsInUpperCase(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("8", "B").replace("6", "G").replace("5", "S").replace("2", "Z")\
+                .replace("1", "L").replace("9", "G").replace("7", "T")
+        return ""
+
+
+class RemoveExtraSpaces(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub(" +", " ", data).strip()
+        return ""
+
+
+class RemoveUserMentions(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub(r'@\w+', '', data)
+        return ""
+
+
+class RemoveUrls(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub(r'http\S+', '', data)
+        return ""
+
+
+class RemoveHashtags(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub("#\\S+", "", data)
+        return ""
+
+
+class RemoveTicks(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("'", "")
+        return ""
+
+
+class RemoveBackTicks(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return data.replace("`", "")
+        return ""
+
+
+class RemovePunctuation(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub('[%s]' % re.escape(string.punctuation), '', data)
+        return ""
+
+
+class RemoveNumbers(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return re.sub(r'\w*\d+\w*', '', data)
+        return ""
+
+
+class RemoveAccents(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            data = data.replace("á", "a").replace("é", "e").replace("í", "i")\
+                .replace("ó", "o").replace("ú", "u").replace("ñ", "n")
+            data = data.replace("Á", "a").replace("É", "e").replace("Í", "i")\
+                .replace("Ó", "o").replace("Ú", "u").replace("Ñ", "N")
+            return data
+        return ""
+
+
+class RemoveStopWords(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=SPANISH_STOP_WORDS)
+        return ""
+
+
+class RemoveArticles(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=ARTICLES)
+        return ""
+
+
+class RemoveEmoticons(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=EMOTICONS)
+        return ""
+
+
+class RemovePronouns(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=PRONOUNS)
+        return ""
+
+
+class RemoveAdverbs(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=ADVERBS)
+        return ""
+
+
+class RemoveConjunctions(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=CONJUNCTIONS)
+        return ""
+
+
+class RemovePrepositions(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=PREPROSITIONS)
+        return ""
+
+
+class RemoveAdjectives(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=ADJECTIVES)
+        return ""
+
+
+class RemoveHtmlTags(CleanDataStrategy):
+    def clean(self, data: str) -> str:
+        if data:
+            cleaner = re.compile('<.*?>')
+            return re.sub(cleaner, '', data)
+        return ""
+
+
+class RemoveEmailAddress(CleanDataStrategy):
+    def clean(self, data: str):
+        if data:
+            return re.sub(r'\S*@\S*\s*', '', data)
+        return ""
+
+
+class ExpandAbbreviations(CleanDataStrategy):
+    def clean(self, data: str):
+        if data:
+            result = data
+            for word in data.split(' '):
+                if word in ABBREVIATIONS:
+                    result = result.replace(word, ABBREVIATIONS[word])
+            return result
+        return ""
+
+
+class RemoveAbbreviations(CleanDataStrategy):
+    def clean(self, data: str):
+        if data:
+            return self.remove_dynamic_tokens(data=data, words=list(ABBREVIATIONS.keys()))
+        return ""
